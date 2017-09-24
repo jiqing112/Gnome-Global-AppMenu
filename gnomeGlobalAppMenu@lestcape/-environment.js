2d49d2cf29d787253d919654fbf540bbfc693403
lestcape <lestcape@gmail.com>
2017-09-24 12:19:06 -0500

    Remove another missing file for the last commit.

--- gnomeGlobalAppMenu@lestcape/-environment.js
+++
@@ -1,9 +0,0 @@
-// -*- mode: js; js-indent-level: 4; indent-tabs-mode: nil -*-
-function init() {
-    // Add some bindings to the global JS namespace; (gjs keeps the web
-    // browser convention of having that namespace be called 'window'.)
-    //patchStIcon();
-}
-
-function finalized() {
-}
